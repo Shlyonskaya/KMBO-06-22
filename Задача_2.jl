cd2fb3066f2b94aaefe72acbb91f1685a7b8a044
Shlyonskaya <115145761+Shlyonskaya@users.noreply.github.com>
2022-12-24 17:51:56 +0300

    Update Задача_2.jl

--- Задача_2.jl
+++ Задача_2.jl
@@ -2,4 +2,23 @@
 ДАНО: Робот - в произвольной клетке поля (без внутренних перегородок и маркеров)    
 РЕЗУЛЬТАТ: Робот - в исходном положении, и все клетки по периметру внешней рамки промаркированы. 
 """
+
 using HorizonSideRobots
+function perimeter!(robot)
+    corner!(robot)
+    for side in (Ost, Nord, West, Sud)
+        while !isborder(robot, side)
+            move!(robot, side)
+            putmarker!(robot)
+        end
+    end
+end
+
+function corner!(robot)
+    while !isborder(robot, Sud)
+        move!(robot, Sud)
+    end
+    while !isborder(robot, West)
+        move!(robot, West)
+    end
+end
