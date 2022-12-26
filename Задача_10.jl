385a9e51db1c2fa7b37f57fcc0b929070b568d60
Shlyonskaya <115145761+Shlyonskaya@users.noreply.github.com>
2022-12-26 15:31:27 +0300

    Update Задача_10.jl

--- Задача_10.jl
+++ Задача_10.jl
@@ -4,65 +4,93 @@
 РЕЗУЛЬТАТ: Робот - в исходном положении, и на всем поле расставлены маркеры в шахматном порядке клетками размера N*N (N-параметр функции), 
 начиная с юго-западного угла 
 """
-function staggered_order_marker!(robot)
-    imax, jmax = r.situation.frame_size
-    num_Sud, num_West = to_corner!(robot)
-    if (num_Sud + num_West) % 2 == 0
-        staggered_order = 0
-    else
-        staggered_order = 1
+function find_corner!(robot) #Функция, направляющая робота в угол и считающая шаги
+    num_steps_Sud=0
+    num_steps_West=0
+    while (!isborder(robot,Sud))
+        move!(robot,Sud)
+        num_steps_Sud+=1
     end
-    side = Ost
-    while !isborder(robot, side)
-        if(staggered_order % 2 == 0)
+    while (!isborder(robot,West))
+        move!(robot,West)
+        num_steps_West+=1
+    end
+    return num_steps_Sud, num_steps_West
+end
+
+function go_to_home!(robot,num_steps_Sud,num_steps_West) #Возвращает робота в исходное положение
+    for _i in 1:num_steps_Sud
+        move!(robot,Nord)
+    end
+    for _i in 1:num_steps_West
+        move!(robot,Ost)
+    end
+end
+
+function chess!(robot,N) #Рисовать
+    while (!isborder(robot,Nord))
+        for _i in 1:N #Рисуем N строк 
+            while (!isborder(robot,Ost))
+                point!(robot,N)
+                moving!(robot,N)
+            end
+            while (!isborder(robot,West))
+                move!(robot,West)
+            end
+                try_move!(robot,Nord)
+        end
+
+        for _i in 1:N
+            while (!isborder(robot,Ost))
+                moving!(robot,N)
+                point!(robot,N)
+            end
+            while (!isborder(robot,West))
+                move!(robot,West)
+            end
+            try_move!(robot,Nord)
+        end
+    end
+end
+
+function point!(robot,N) #Рисуем часть точки
+        for _i in 1:N
             putmarker!(robot)
-        end
-        move!(robot, side)
-        staggered_order += 1
-        if(isborder(robot, side) && !isborder(robot, Nord))
-            if(staggered_order % 2 == 0)
-                putmarker!(robot)
+            if (try_move!(robot,Ost)==false)
+                return
             end
-            move!(robot, Nord)
-            staggered_order += 1
-            if(staggered_order % 2 == 0)
-                putmarker!(robot)
-            end
-            side = inverse(side)
         end
+end
+
+function moving!(robot,N) #Расстояние между частями точки
+    for _i in 1:N
+        try_move!(robot,Ost)
     end
-    putmarker!(robot)
-    if jmax % 2 == 0
-        num_West = jmax - 1 - num_West
-        side_back = West
+end
+
+function try_move!(robot,side) 
+#делает попытку одного шага в заданном направлении и  возвращает true, в случае, если это возможно, и false - в  противном случае (робот остается в исходном положении) 
+    if (!isborder(robot,side))
+        move!(robot,side)
+        return true
     else
-        side_back = Ost
-    end
-    num_Sud = imax - 1 - num_Sud
-    along!(robot, num_Sud, num_West, side_back)
-end
-
-function to_corner!(robot)
-    num_West = 0
-    num_Sud = 0
-    while !isborder(robot, Sud)
-        move!(robot, Sud)
-        num_Sud += 1
-    end
-    while !isborder(robot, West)
-        move!(robot, West)
-        num_West += 1
-    end
-    return num_Sud, num_West
-end
-
-function along!(robot, num_Sud, num_Ost, side_back)
-    for _i in 1:num_Sud
-        move!(robot, Sud)
-    end
-    for _i in 1:num_Ost
-        move!(robot,side_back)
+        return false
     end
 end
 
-inverse(side::HorizonSide) = HorizonSide((Int(side) +2)% 4)
+function go_to_corner!(robot) #уходим снова в юго-западный угол
+    while (!isborder(robot,Sud))
+        move!(robot,Sud)
+    end
+
+    while (!isborder(robot,West))
+        move!(robot,West)
+    end
+end
+
+function solve!(robot,N::Int)
+   num_steps_Sud,num_steps_West=find_corner!(robot)
+   chess!(robot,N)
+   go_to_corner!(robot)
+   go_to_home!(robot,num_steps_Sud,num_steps_West)
+end
