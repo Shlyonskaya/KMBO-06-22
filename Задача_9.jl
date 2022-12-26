d8432547357e00f1b40acbaedf51801d96de2397
Shlyonskaya <115145761+Shlyonskaya@users.noreply.github.com>
2022-12-26 13:10:59 +0300

    Update Задача_9.jl

--- Задача_9.jl
+++ Задача_9.jl
@@ -3,3 +3,26 @@
 
 РЕЗУЛЬТАТ: Робот - в исходном положении, на всем поле расставлены маркеры в шахматном порядке, причем так, чтобы в клетке с роботом находился маркер  
 """
+
+function find_marker!(robot)
+    num_step=1
+    side=Nord
+    while !ismarker(robot)
+        for _i in 1:2   
+            marker_is_find!(robot ,side, num_step)
+            side=next(side)
+        end
+        num_step=num_step+1
+    end
+end
+
+function marker_is_find!(robot, side, num_step)  
+    for _i in 1:num_step   
+        if !ismarker(r)
+            move!(robot,side)
+        else
+            break
+        end 
+    end
+end
+next(side::HorizonSide) = HorizonSide((Int(side) +1)% 4)
